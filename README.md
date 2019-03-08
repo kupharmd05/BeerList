b4fc8ee074d96dce1bfe4a8a397f1346daf2b8f6
kupharmd05 <kupharmd05@gmail.com>
2019-03-08 15:43:06 -0600

    updated readme and changed buttons

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # BeerList
+
+## The Assignment
+Use Handlebars to render front end data using the MVC model.  We created our own ORM (Object-relational mapping) and use Express.js for our server setup. 
+
+## Demo
+Please click[here]()
+
+## Install
+To install git clone https://github.com/kupharmd05/BeerList.git
+
+cd BeerList
+
+npm install
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-insert into beers (beer, devoured) values ("Bud Light", true);
+insert into beers (beer, devoured) values ("Bud Light", false);
 insert into beers (beer, devoured) values ("Boulevard Wheat", true);
-insert into beers (beer, devoured) values ("Boulevard Tank7", true);
+insert into beers (beer, devoured) values ("Boulevard Tank 7", false);
 
 select * from beers;
